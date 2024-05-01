6bd0ebb999ab9487c89580c25324558197c74a4d
AlexeyMikhylov <alexeymikhylov02@gmail.com>
2024-05-01 12:55:07 +0300

    ./

--- Source.c
+++ Source.c
@@ -1,29 +1,10 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	choice();
+	switchcase();
 	return 0;
-}
-
-int choice(void)
-{
-	int num;
-	puts("1 - switch case; 2 - pointer");
-	scanf("%d", &num);
-	switch (num)
-	{
-	case 0:
-		exit(0);
-	case 1:
-		switchcase();
-		choice();
-	case 2:
-		pointer();
-		choice();
-	default:
-		break;
-	}
 }
 
 int operations(char o)
@@ -93,91 +74,6 @@ int switchcase()
 		else
 		{
 			operations(o);
-		}
-	}
-}
-
-int addup(float a, float b)
-{
-	return (a + b);
-}
-
-int subtract(float a, float b)
-{
-	return (a - b);
-}
-
-int multiply(float a, float b)
-{
-	return (a * b);
-}
-
-int divide(float a, float b)
-{
-	if (b == 0)
-	{
-		puts("cannot be divided by 0");
-	}
-	else
-		return (a / b);
-}
-
-int pointer() 
-{
-	char o;
-	float a, b,c;
-	float (*operation[])(float, float) = { ['+'] = addup,['-'] = subtract,['*'] = multiply,['/'] = divide };
-	while (10 > 0)
-	{
-		puts("\nwhat you want to do?\n");
-		puts("enter:\n+ to add up;\n- to subtract;\n* to multiply;\n/ to divide;\n 0 to exit;\n");
-		scanf(" %c", &o);
-
-		//если было введено больше 1 сивола
-		if (getchar() != '\n') {
-			printf("Error\n");
-
-			// Очистка буфера ввода
-			while (getchar() != '\n');
-
-			continue; // Повторяем ввод заново
-		}
-
-		if (o != '+' && o != '-' && o != '*' && o != '/' && o != '0')
-		{
-			puts("\nerror");
-			pointer();
-		}
-		if (o == '0')
-		{
-			//choice();
-			exit(0);
-		}
-		puts("\nenter the first number: ");
-		scanf("%f", &a);
-		puts("enter the second number: ");
-		scanf("%f", &b);
-		c = operation[o](a, b);
-		if (o == '+')
-		{
-			printf("\n%g + %g = %g\n", a, b, c);
-		}
-		if (o == '-')
-		{
-			printf("\n%g - %g = %g\n", a, b, c);
-		}
-		if (o == '*')
-		{
-			printf("\n%g * %g = %g\n", a, b, c);
-		}
-		if (o == '/')
-		{
-			if (b != 0)
-			{
-				printf("\n%g / %g = %g\n", a, b, c);
-			}
-			else
-				puts("error");
 		}
 	}
 }
